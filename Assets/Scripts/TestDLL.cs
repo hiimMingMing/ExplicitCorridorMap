d4faa630e0c7ca6050e52b467dac4cb234e170f7
Nguyen Quoc Bao <quocbao100@gmail.com>
2019-01-07 18:27:39 +0700

    a

---
+++ Assets/Scripts/TestDLL.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using SharpBoostVoronoi;
+
+public class TestDLL : MonoBehaviour
+{
+
+    void Start()
+    {
+        BoostVoronoi boostVoronoi = new BoostVoronoi();
+
+    }
+
+    void Update()
+    {
+    }
+}
