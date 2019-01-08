7c3764c73c14bc8d6748de53f2027870503f7a38
Nguyen Quoc Bao <quocbao100@gmail.com>
2019-01-08 08:49:36 +0700

    a

--- Assets/Scripts/OpenVoronoi.cs
+++ Assets/Scripts/OpenVoronoi.cs
@@ -22,4 +22,8 @@ public class OpenVoronoi : MonoBehaviour
     {
         
     }
+    void DrawVoronoi(VoronoiDiagram voronoiDiagram)
+    {
+
+    }
 }
--- Assets/Scripts/OpenVoronoiCSharp/DataStructures/KdPoint.cs
+++ Assets/Scripts/OpenVoronoiCSharp/DataStructures/KdPoint.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using OpenVoronoiCSharp.Internals;
 namespace OpenVoronoiCSharp.DataStructures
 {
     class KdPoint
