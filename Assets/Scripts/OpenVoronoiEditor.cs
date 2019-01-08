da85f317113ab16b803f859899e5ffbb5f4bf8b7
Nguyen Quoc Bao <quocbao100@gmail.com>
2019-01-08 18:11:46 +0700

    remove v,e outside box

--- Assets/Scripts/OpenVoronoiEditor.cs
+++ Assets/Scripts/OpenVoronoiEditor.cs
@@ -11,6 +11,7 @@ public class OpenVoronoiEditor : Editor
     //private VoronoiDiagram vd;
     public float vWidth = 0.01f;
     VoronoiDiagram vd;
+    Rect box;
     public override void OnInspectorGUI()
     {
         DrawDefaultInspector();
@@ -21,28 +22,34 @@ public class OpenVoronoiEditor : Editor
         if (GUILayout.Button("Bake"))
         {
             var watch = System.Diagnostics.Stopwatch.StartNew();
-            float smallSize = 4f;
+            float smallSize = 3f;
             float largeSize = 10f;
             vd = new VoronoiDiagram(10);
             var v1 = vd.insert_point_site(new Point(-smallSize, smallSize));
             var v2 = vd.insert_point_site(new Point(smallSize, smallSize));
             var v3 = vd.insert_point_site(new Point(smallSize, -smallSize));
-            var v4 = vd.insert_point_site(new Point(-smallSize, -smallSize));
+            var v4 = vd.insert_point_site(new Point(2*smallSize, -smallSize));
+            var v5 = vd.insert_point_site(new Point(2*smallSize, -2*smallSize));
+            var v6 = vd.insert_point_site(new Point(-smallSize, -2*smallSize));
             var v11 = vd.insert_point_site(new Point(-largeSize, largeSize));
             var v22 = vd.insert_point_site(new Point(largeSize, largeSize));
             var v33 = vd.insert_point_site(new Point(largeSize, -largeSize));
             var v44 = vd.insert_point_site(new Point(-largeSize, -largeSize));
 
+            box = new Rect(-largeSize,-largeSize,largeSize*2+0.1f,largeSize*2+0.1f);
+
             vd.insert_line_site(v1, v2);
             vd.insert_line_site(v2, v3);
             vd.insert_line_site(v3, v4);
-            vd.insert_line_site(v4, v1);
+            vd.insert_line_site(v4, v5);
+            vd.insert_line_site(v5, v6);
+            vd.insert_line_site(v6, v1);
             vd.insert_line_site(v11, v22);
             vd.insert_line_site(v22, v33);
             vd.insert_line_site(v33, v44);
             vd.insert_line_site(v44, v11);
 
-            //vd.filter(new MedialAxisFilter());
+            vd.filter(new MedialAxisFilter());
 
             watch.Stop();
             Debug.Log(watch.ElapsedMilliseconds);
@@ -70,6 +77,9 @@ public class OpenVoronoiEditor : Editor
         Vertex trg = e.target;
         Point src_p = src.position;
         Point trg_p = trg.position;
+        if (!box.Contains(new Vector2((float)src_p.x, (float)src_p.y))||
+            !box.Contains(new Vector2((float)trg_p.x, (float)trg_p.y)) )
+            return;
 
 
         List<Point> points = new List<Point>();
@@ -114,6 +124,8 @@ public class OpenVoronoiEditor : Editor
     {
         //if (v.type != VertexType.SEPPOINT) return;
         Point p = v.position;
+        if (!box.Contains(new Vector2((float)p.x, (float)p.y))) return;
+
         Handles.color = Color.yellow;
         Handles.DrawSolidDisc(new Vector3((float)p.x, (float)p.y), new Vector3(0, 0, 1), vWidth * 1.5f);
         Handles.Label(new Vector3((float)p.x, (float)p.y), "x:"+p.x + "\n" +"y:"+ p.y);
