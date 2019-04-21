542556f90ad41ff6d5aeb069a5e06fa7fb2d23d9
Nguyễn Đình Hào <dinhhaobk97@gmail.com>
2019-04-21 11:21:37 +0700

    Add ODPA case unaffected path when delete obs

--- Assets/Scripts/DynamicReplanning/DynamicReplanningComponent.cs
+++ Assets/Scripts/DynamicReplanning/DynamicReplanningComponent.cs
@@ -11,14 +11,15 @@ namespace ExplicitCorridorMap
         private ECMMap ecmMap;
         public bool usingODPA = false;
         public KeyCode defaultAddKeyCode;
+        public KeyCode defaultDeleteKeyCode;
         void Awake() { }
         void Start() {
             if (obstaclePrefab == null) {
                 Debug.LogError("Please add default obstacle");
             }
             ecmMap = FindObjectOfType<ECMMap>();
-            if (ecmMap==null) {
-                Debug.LogError("Please create  EcmMap");
+            if (ecmMap == null) {
+                Debug.LogError("Please create EcmMap");
             }
         }
 
@@ -67,7 +68,7 @@ namespace ExplicitCorridorMap
         //GameAgents version
         public  void HandleDynamicEvent()
         {
-           
+            //Set key for adding obstacle
             if (Input.GetKeyDown(defaultAddKeyCode))
             {
                 //Add a dynamic obstacle
@@ -86,11 +87,11 @@ namespace ExplicitCorridorMap
                 mousePosition = new Vector2(_position.x, _position.y);
                 
                 Vector3 position = new Vector3(mousePosition.x, 0, mousePosition.y);
-                addObstacle(obstaclePrefab, position);
-              
-            
+                addObstacle(obstaclePrefab, position);                        
             }
-            else if (Input.GetKeyDown(KeyCode.D))
+
+            //Set key for deleting obstacle
+            else if (Input.GetKeyDown(defaultDeleteKeyCode))
             {
                 Vector2 mousePosition;
                 float rayDistance;
@@ -105,11 +106,12 @@ namespace ExplicitCorridorMap
                 }
 
                 mousePosition = new Vector2(_position.x, _position.y);
-                //Delete an obstacle
-                
+
+                //Delete an obstacle          
                 int ID = FindObstacleID(ecmMap.ECMGraph, mousePosition);
                 if (ID == -1) return;
                 var deleteObs = ecmMap.ECMGraph.Obstacles[ID];
+                var obsRect = ConvertObsToRect(deleteObs);
                 Destroy(deleteObs.GameObject);
                 var rvoID = deleteObs.RvoID;
                 ecmMap.ECMGraph.DeletePolygonDynamic(ID);
@@ -119,13 +121,19 @@ namespace ExplicitCorridorMap
                 //Handle the path
                 foreach (var item in ecmMap.AgentMap)
                 {
-
                     GameAgent player = item.Value;
                     if (player.WayPointList.Count > 0)
                     {
-                        List<Vector2> newPath;
+                        List<Vector2> newPath = new List<Vector2>();
 
-                        newPath = PathFinding.FindPath(ecmMap.ECMGraph, player.RadiusIndex, player.transform.position, player.WayPointList[player.WayPointList.Count - 1]);
+                        if (!usingODPA)
+                        {
+                            newPath = PathFinding.FindPath(ecmMap.ECMGraph, player.RadiusIndex, player.transform.position, player.WayPointList[player.WayPointList.Count - 1]);
+                        }
+                        else
+                        {
+                            newPath = DynamicFindPathByODPAVer1(ecmMap.ECMGraph, player.RadiusIndex, player.transform.position, player.WayPointList[player.WayPointList.Count - 1], player.WayPointList, obsRect);
+                        }
 
                         if (newPath.Count > 0)
                         {
@@ -139,11 +147,117 @@ namespace ExplicitCorridorMap
 
         //Case 1: Unaffected path (When delete an obstacle)
         //R_area is area changed by add/delete obstacle
-        public static List<Vector2> DynamicFindPathByODPAVer1(ECM ecm, int radiusIndex, Vector2 startPosition, Vector2 endPosition, RectInt R_area)
+        public static List<Vector2> DynamicFindPathByODPAVer1(ECM ecm, int radiusIndex, Vector2 startPosition, Vector2 endPosition, List<Vector2> wayPointList, Rect R_area)
         {
             List<Vector2> newPath = new List<Vector2>();
 
+            var pathPortals = new List<Portal>();
+            var radius = ecm.AgentRadius[radiusIndex];
+
+            var startEdge = ecm.GetNearestEdge(ref startPosition, radius);
+            var endEdge = ecm.GetNearestEdge(ref endPosition, radius);
+
+            //Run ODPA*
+            var oldPath = FindOldVertexPathVer1(ecm, wayPointList, startPosition); //oldPath TG-
+
+            var vertexList = DynamicFindPathVertexVer1(ecm, radiusIndex, startEdge.Start, startEdge.End, endEdge.Start, endEdge.End, startPosition, endPosition, oldPath, R_area);
+            if (vertexList.Count == 0) return newPath;
+
+            //Convert to path
+            var edgeList = PathFinding.ConvertToEdgeList(vertexList);
+            pathPortals = PathFinding.ComputePortals(radiusIndex, edgeList, startPosition, endPosition);
+
+            newPath = PathFinding.GetShortestPath(pathPortals).ConvertAll(x => x.Point);
             return newPath;
+        }
+
+        //ODPA* algorithm case 1: Unaffected path
+        public static List<Vertex> DynamicFindPathVertexVer1(ECM ecm, int radiusIndex, Vertex start1, Vertex start2, Vertex end1, Vertex end2, Vector2 startPosition, Vector2 endPosition, List<Vertex> oldPath, Rect R_area)
+        {
+            List<Vertex> result = new List<Vertex>();
+            var radius = ecm.AgentRadius[radiusIndex];
+
+            var openSet = new HashSet<Vertex>();
+            openSet.Add(start1);
+            openSet.Add(start2);
+            var closeSet = new HashSet<Vertex>();
+            var cameFrom = new Dictionary<Vertex, Vertex>();
+            var gScore = new Dictionary<Vertex, float>();
+            var fScore = new Dictionary<Vertex, float>();
+            gScore[start1] = PathFinding.HeuristicCost(startPosition, start1.Position);
+            fScore[start1] = PathFinding.HeuristicCost(start1.Position, endPosition);
+            gScore[start2] = PathFinding.HeuristicCost(startPosition, start2.Position);
+            fScore[start2] = PathFinding.HeuristicCost(start2.Position, endPosition);
+
+            //ODPA
+            bool checkAll;
+            var visitedR = new Dictionary<Vertex, bool>();
+            visitedR[start1] = false;
+            visitedR[start2] = false;
+            var rWorse = new Dictionary<Vertex, bool>();
+            rWorse[start1] = false;
+            rWorse[start2] = false;
+            float oldCost = 0;
+            for (int i = 0; i < oldPath.Count - 1; i++)
+                oldCost += PathFinding.HeuristicCost(oldPath[i].Position, oldPath[i + 1].Position); //oldCost path TG-
+            //
+
+            while (openSet.Count != 0)
+            {
+                var current = PathFinding.LowestFScore(openSet, fScore);
+                if (current.Equals(end1) || current.Equals(end2)) return PathFinding.RecontructPath(cameFrom, current);
+                openSet.Remove(current);
+                closeSet.Add(current);
+
+                //ODPA
+                if (R_area.Contains(current.Position))
+                    visitedR[current] = true;
+                else if (cameFrom.ContainsKey(current))
+                {
+                    visitedR[current] = visitedR[cameFrom[current]];
+                    rWorse[current] = rWorse[cameFrom[current]];
+                }
+
+                if (!oldPath.Contains(current) && visitedR[current])
+                    checkAll = true;
+                else
+                {
+                    Vector2 R_point = R_area.center; //Center point in R_area
+                    if (PathFinding.HeuristicCost(startPosition, current.Position) + PathFinding.HeuristicCost(current.Position, R_point)
+                        + PathFinding.HeuristicCost(R_point, endPosition) > oldCost) //g(V) + h'(V,R) + h'(G,R) > cost([TG]-)
+                    {
+                        rWorse[current] = true;
+                    }
+                    else
+                    {
+                        rWorse[current] = false;
+                    }
+
+                    if (!rWorse[current])
+                        checkAll = true;
+                    else if (oldPath.Contains(current))
+                        checkAll = false;
+                    else continue;
+                } //
+
+                foreach (var edge in current.Edges)
+                {
+                    var neigborVertex = edge.End;
+                    if (closeSet.Contains(neigborVertex) || edge.HasEnoughClearance(radius)) continue;
+
+                    //ODPA
+                    if (!checkAll && !oldPath.Contains(neigborVertex)) continue; //
+
+                    var tentativeGScore = gScore[current] + edge.Length;
+                    if (!openSet.Contains(neigborVertex)) openSet.Add(neigborVertex);
+                    else if (tentativeGScore >= gScore[neigborVertex]) continue;
+                    cameFrom[neigborVertex] = current;
+                    visitedR[neigborVertex] = rWorse[neigborVertex] = false; //ODPA
+                    gScore[neigborVertex] = tentativeGScore;
+                    fScore[neigborVertex] = tentativeGScore + PathFinding.HeuristicCost(neigborVertex.Position, endPosition);
+                }
+            }
+            return result;
         }
 
         //Case 2: Affected path (When add new obstacle)
@@ -164,7 +278,7 @@ namespace ExplicitCorridorMap
 
             //
             //Step 2: Go to endPosition using ODPA*
-            var oldPath = FindOldVertexPath(ecm, wayPointList, lastAffectPoint); //oldPath BG-
+            var oldPath = FindOldVertexPathVer2(ecm, wayPointList, lastAffectPoint); //oldPath BG-
 
             var midEdge = ecm.GetNearestEdge(ref lastAffectPoint, radius);
             var endEdge = ecm.GetNearestEdge(ref endPosition, radius);
@@ -220,7 +334,7 @@ namespace ExplicitCorridorMap
                 closeSet.Add(current);
 
                 //ODPA
-                //if (cameFrom.ContainsKey(current)) rWorse[cameFrom[current]] = rWorse[current];
+                if (cameFrom.ContainsKey(current)) rWorse[current] = rWorse[cameFrom[current]];
                 if (!oldPath.Contains(current))
                     checkAll = true;
                 else
@@ -251,6 +365,7 @@ namespace ExplicitCorridorMap
                     if (!openSet.Contains(neigborVertex)) openSet.Add(neigborVertex);
                     else if (tentativeGScore >= gScore[neigborVertex]) continue;
                     cameFrom[neigborVertex] = current;
+                    rWorse[neigborVertex] = false;
                     gScore[neigborVertex] = tentativeGScore;
                     fScore[neigborVertex] = tentativeGScore + PathFinding.HeuristicCost(neigborVertex.Position, endPosition);
                 }
@@ -290,8 +405,30 @@ namespace ExplicitCorridorMap
             return list;
         }
 
-        //Find old list of vertex 
-        public static List<Vertex> FindOldVertexPath(ECM ecm, List<Vector2> wayPointList, Vector2 startPosition)
+        //Find old list of vertex case: Unaffected path
+        public static List<Vertex> FindOldVertexPathVer1(ECM ecm, List<Vector2> wayPointList, Vector2 currentPosition)
+        {
+            List<Vertex> listVertex = new List<Vertex>();
+            Vector2 startPosition = ecm.GetNearestEdge(currentPosition).Start.Position;
+
+            for (int i = wayPointList.Count - 1; i > 0; i--)
+            {
+                var edge = ecm.GetNearestEdge(wayPointList[i]);
+                if (edge != null)
+                {
+                    listVertex.Add(edge.End);
+                    listVertex.Add(edge.Start);
+                }
+                if (wayPointList[i].Equals(startPosition)) break;
+            }
+            var newListVertex = DRMath.RemoveDuplicate(listVertex);
+            newListVertex.Reverse();
+
+            return newListVertex;
+        }
+
+        //Find old list of vertex case: Affected path
+        public static List<Vertex> FindOldVertexPathVer2(ECM ecm, List<Vector2> wayPointList, Vector2 startPosition)
         {
             List<Vertex> listVertex = new List<Vertex>();
 
@@ -346,6 +483,17 @@ namespace ExplicitCorridorMap
                 }
             }
             return -1;
+        }
+
+        //Convert Obstacle to RectInt
+        public static Rect ConvertObsToRect(Obstacle obs)
+        {
+            float xMin = obs.Points[0].x;
+            float yMin = obs.Points[0].y;
+            float width = Distance.ComputeDistanceBetweenPoints(obs.Points[0], obs.Points[3]);
+            float height = Distance.ComputeDistanceBetweenPoints(obs.Points[0], obs.Points[1]);
+
+            return new Rect(xMin, yMin, width, height);
         }
     }
 }
